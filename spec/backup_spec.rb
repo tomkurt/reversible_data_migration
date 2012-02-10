04d71b022492070e867e836e19493ac31a28cf04
Tom Maeckelberghe <tom.maeckelberghe@gmail.com>
2012-02-10 19:41:06 +0100

    adds test for transaction restore

--- spec/backup_spec.rb
+++ spec/backup_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 module ReversableDataMigration
   RAILS_ROOT = File.dirname(__FILE__)
 end 
+
 describe ReversableDataMigration do
   
   before :each do
@@ -75,5 +76,78 @@ describe ReversableDataMigration do
 
     RemoveStateFromProduct.up
     RemoveStateFromProduct.down
+  end
+
+  it "should delete created records" do
+
+    class RemoveStateFromProduct < ActiveRecord::Migration
+      def self.up
+        backup [{:id => Product.create.id}]
+      end
+
+      def self.down
+        destroy_created_records Product
+      end
+    end
+
+    RemoveStateFromProduct.up
+    Product.count.should == 3
+    RemoveStateFromProduct.down
+    Product.count.should == 2
+  end
+
+
+  it "should remove backup files after everything is recovered" do 
+    class RemoveStateFromProduct < ActiveRecord::Migration
+      def self.up
+        product = Product.first
+        backup [{:id => product.id, :state => product.state}]
+        product.update_attribute("state", "something")
+        backup [{:id => Product.create.id}], "second_backup"
+      end
+
+      def self.down
+        restore_batch do
+          restore Product
+          destroy_created_records Product, "second_backup"
+        end
+      end
+    end
+
+    RemoveStateFromProduct.up
+    RemoveStateFromProduct.down 
+    
+    backup_files.should ==  []
+  end
+
+
+  it "should keep backup files if an error occurs" do 
+    class RemoveStateFromProduct < ActiveRecord::Migration
+      def self.up
+        product = Product.first
+        backup [{:id => product.id, :state => product.state}]
+        product.update_attribute("state", "something")
+        backup [{:id => Product.create.id}], "second_backup"
+      end
+
+      def self.down
+        restore_batch do
+          restore Product
+          raise "error"
+          destroy_created_records Product, "second_backup"
+        end
+      end
+    end
+
+    RemoveStateFromProduct.up
+    RemoveStateFromProduct.down rescue nil
+    
+    backup_files.should ==  ["remove_state_from_product.yml", "second_backup.yml"]
+  end
+
+private
+
+  def backup_files
+    `ls #{File.join(File.dirname(__FILE__),  'db/migrate/backup_data')}`.split("\n")
   end
 end
